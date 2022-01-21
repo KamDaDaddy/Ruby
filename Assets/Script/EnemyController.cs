a89e714790a96d8c3e32ca59a99e7649b516068e
KamDaDaddy <97680346+KamDaDaddy@users.noreply.github.com>
2022-01-21 08:13:45 -0500

    Initial commit

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+    
+    Rigidbody2D rigidbody2D;
+    float timer;
+    int direction = 1;
+    Animator animator;
+    
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+        animator = GetComponent<Animator>();
+    }
+    void Update() 
+    {
+    timer -= Time.deltaTime;
+
+    if(timer < 0)
+    {
+        direction = -direction;
+        timer = changeTime;
+    }
+
+
+}
+    void FixedUpdate() 
+    {
+        Vector2 position = rigidbody2D.position;
+       
+       if (vertical)
+       {
+           animator.SetFloat("Move X", 0);
+           animator.SetFloat("Move Y", direction);
+
+           position.y = position.y + Time.deltaTime * speed;
+       }
+       else
+       {
+           animator.SetFloat("Move X", direction);
+           animator.SetFloat("Move Y", 0);
+
+           position.x = position.x + Time.deltaTime * speed;
+       }
+        
+        rigidbody2D.MovePosition(position);
+
+    }
+void OnCollisionEnter2D(Collision2D other) 
+{
+    RubyController player = other.gameObject.GetComponent<RubyController>();
+
+    if(player != null)
+    {
+        player.ChangeHealth(-1);
+    }    
+}
+
+
+}
+
